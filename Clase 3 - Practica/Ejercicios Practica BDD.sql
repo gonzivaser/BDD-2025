4f8261b4eb10a127b467e1ffcf00965f45d216f0
gonzivaser <gonzalovaserman@gmail.com>
2025-04-16 17:21:00 -0300

    Hecho ej 20

--- Clase 3 - Practica/Ejercicios Practica BDD.sql
+++ Clase 3 - Practica/Ejercicios Practica BDD.sql
@@ -251,3 +251,22 @@ GROUP BY
 ORDER BY
     fechaUltimaCompra DESC
 
+
+-- EJ 20: Se desea obtener la cantidad de clientes por cada state y city, donde los clientes contengan el string  
+-- ‘ts’ en el nombre de compañía, el código postal este entre 93000 y 94100 y la ciudad no sea 'Mountain View'. Se 
+-- desea el listado ordenado por ciudad
+SELECT 
+    state, 
+    city, 
+    COUNT(*) AS cantidad_clientes
+FROM 
+    customer 
+WHERE 
+    company LIKE '%ts' AND 
+    zipcode BETWEEN '93000' AND '94100' AND 
+    city != 'Mountain View'
+GROUP BY 
+    state, 
+    city
+ORDER BY 
+    city ASC
